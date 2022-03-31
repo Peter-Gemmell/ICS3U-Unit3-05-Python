2b8deaa075c8d37dc9d6e77fc6581f916eafa4a3
peter.gemmell <peter.gemmell@mths.ca>
2022-03-31 01:46:57 +0000

    aiwdaw

---
+++ month_number.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+
+# Created by Peter Gemmell
+# Created on March 2022
+# this function turns # 1-12 to their respective numbers
+def main():
+    # this function turns # 1-12 to their respective numbers
+
+    # input
+    month_number = int(input("Enter a number of a month: "))
+    print("")
+
+    # process & output
+    if month_number == 1:
+        print("The month is January")
+    elif month_number == 2:
+        print("The month is February")
+    elif month_number == 3:
+        print("The month is March")
+    elif month_number == 4:
+        print("The month is April")
+    elif month_number == 5:
+        print("The month is May")
+    elif month_number == 6:
+        print("The month is June")
+    elif month_number == 7:
+        print("The month is July")
+    elif month_number == 8:
+        print("The month is August")
+    elif month_number == 9:
+        print("The month is September")
+    elif month_number == 10:
+        print("The month is October")
+    elif month_number == 11:
+        print("The month is November")
+    elif month_number == 12:
+        print("The month is December")
+    else:
+        print("{} Is an Invalid input. Not between 1-12.".format(month_number))
+        
+    print("\nDone.")
+
+
+if __name__ == "__main__":
+    main()
